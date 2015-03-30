98b0f1bb8a31f59c289dbf9c6900cc647cbbdfb2
zhanglm10 <zhanglm10@gmail.com>
2015-03-30 10:01:49 -0500

    add coverage serializer

---
+++ src/tracer/coverage/io/CoverageSerializer.java
@@ -0,0 +1,28 @@
+package tracer.coverage.io;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+import tracer.faulttracer.utils.FaultTracerProperties;
+
+public class CoverageSerializer {
+	public static void serialize(String type,
+			Map<String, Map<String, Integer>> trace) throws IOException {
+		BufferedWriter writer = new BufferedWriter(new FileWriter(
+				FaultTracerProperties.TRACER_DIR + File.separator + type
+						+ ".dat"));
+		for (String test : trace.keySet()) {
+			writer.write(test + " ");
+			for (String elem : trace.get(test).keySet()) {
+				writer.write(elem + "-" + trace.get(test).get(elem) + " ");
+			}
+			writer.write("\n");
+		}
+		writer.flush();
+		writer.close();
+	}
+
+}
--- src/tracer/coverage/io/MethodTraceLoader.java
+++ src/tracer/coverage/io/MethodTraceLoader.java
@@ -1,13 +1,14 @@
 package tracer.coverage.io;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 import tracer.coverage.core.Properties;
 
 public class MethodTraceLoader {
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		Map<String, Map<String, Integer>> trace=TracerUtils.loadMethodTracesFromDirectory(Properties.TRACER_METH_FILES);
 		for(String test:trace.keySet()){
 			System.out.println("<Test>"+test);
@@ -15,6 +16,7 @@ public class MethodTraceLoader {
 				System.out.println(elem+"-"+trace.get(test).get(elem));
 			}
 		}
+		CoverageSerializer.serialize("method-coverage", trace);
 	}
 
 }
